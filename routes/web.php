4c6386cec87ea9193fff6ce0c1ed41480e68f444
fxdavidh <fxdavidh@gmail.com>
2021-12-13 22:37:50 +0700

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -22,34 +22,31 @@ Route::get('/', function () {
     return redirect(route('getBooks'));
 });
 
-Route::post('/create-book', bookController . '@createBook')->name('createBook');
-Route::get('/create-book', genreController . '@getGenreOnBookCreate')->name('getGenreOnBookCreate');
-Route::get('/update-book/{id}', bookController . '@viewUpdateBook')->name('viewUpdateBook');
-Route::get('/view-book/{id}', bookController . '@viewBook')->name('viewBook');
-Route::patch('/update-book/{id}', bookController . '@updateBook')->name('updateBook');
-Route::delete('/delete-book/{id}', bookController . '@deleteBook')->name('deleteBook');
-Route::get('/get-books', bookController . '@getBooks')->name('getBooks');
-Route::get('/get-books-by-filter', bookController . '@getBooksByFilter')->name('getBooksByFilter');
-
-Route::post('/create-genre', genreController . '@createGenre')->name('createGenre');
-Route::get('/create-genre', genreController . '@viewGenreCreate')->name('viewGenreCreate');
-Route::get('/update-genre/{id}', genreController . '@viewUpdateGenre')->name('viewUpdateGenre');
-Route::patch('/update-genre/{id}', genreController . '@updateGenre')->name('updateGenre');
-Route::delete('/delete-genre/{id}', genreController . '@deleteGenre')->name('deleteGenre');
-Route::get('/get-genres', genreController . '@getGenre')->name('getGenres');
-
-Route::get('/update-users/{id}', userController . '@viewUpdateUser')->name('viewUpdateUser');
-Route::get('/get-users', userController . '@getUsers')->name('getUsers');
-Route::get('/get-user/{id}', userController . '@getUserByUserId')->name('getUserByUserId');
-Route::patch('/update-user/{id}', userController . '@updateUser')->name('updateUser');
-Route::delete('/delete-user/{id}', userController . '@deleteUser')->name('deleteUser');
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('/get-books', bookController . '@getBooks')->name('getBooks');
+Route::get('/get-books-by-filter', bookController . '@getBooksByFilter')->name('getBooksByFilter');
+Route::get('/view-book/{id}', bookController . '@viewBook')->name('viewBook');
 
 Route::group(['middleware' => 'App\Http\Middleware\IsAdmin', 'prefix' => 'admin'], function () {
+    Route::get('/update-users/{id}', userController . '@viewUpdateUser')->name('viewUpdateUser');
+    Route::get('/get-users', userController . '@getUsers')->name('getUsers');
+    Route::get('/get-user/{id}', userController . '@getUserByUserId')->name('getUserByUserId');
+    Route::patch('/update-user/{id}', userController . '@updateUser')->name('updateUser');
+    Route::delete('/delete-user/{id}', userController . '@deleteUser')->name('deleteUser');
+
+    Route::post('/create-genre', genreController . '@createGenre')->name('createGenre');
+    Route::get('/create-genre', genreController . '@viewGenreCreate')->name('viewGenreCreate');
+    Route::get('/update-genre/{id}', genreController . '@viewUpdateGenre')->name('viewUpdateGenre');
+    Route::patch('/update-genre/{id}', genreController . '@updateGenre')->name('updateGenre');
+    Route::delete('/delete-genre/{id}', genreController . '@deleteGenre')->name('deleteGenre');
+    Route::get('/get-genres', genreController . '@getGenre')->name('getGenres');
+
+    Route::post('/create-book', bookController . '@createBook')->name('createBook');
+    Route::get('/create-book', genreController . '@getGenreOnBookCreate')->name('getGenreOnBookCreate');
+    Route::get('/update-book/{id}', bookController . '@viewUpdateBook')->name('viewUpdateBook');
+    Route::patch('/update-book/{id}', bookController . '@updateBook')->name('updateBook');
+    Route::delete('/delete-book/{id}', bookController . '@deleteBook')->name('deleteBook');
 });
 
 Route::group(['middleware' => 'App\Http\Middleware\IsMember', 'prefix' => 'member'], function () {
